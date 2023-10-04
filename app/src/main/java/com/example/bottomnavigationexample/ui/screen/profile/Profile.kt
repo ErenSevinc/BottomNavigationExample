af39b0905341a465918f927738f107b1d2a2c269
erensevinc1414@gmail.com <erensevinc1414@gmail.com>
2023-10-04 07:57:13 +0300

    bug fix

--- app/src/main/java/com/example/bottomnavigationexample/ui/screen/profile/Profile.kt
+++ app/src/main/java/com/example/bottomnavigationexample/ui/screen/profile/Profile.kt
@@ -1,38 +1,52 @@
 package com.example.bottomnavigationexample.ui.screen.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import androidx.navigation.NavController
 
-const val mask = "+90 (xxx) xxx xx xx"
-const val maskChar = 'x'
-//const val mask = "XXXX-XXXX-XXXX-XXXX"
-//const val maskChar = 'x'
+const val mask = "+90 (###) ### ## ##"
+const val maskChar = '#'
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navController: NavController) {
     var phoneNumber by rememberSaveable { mutableStateOf("") }
-    Column {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
         PhoneField(
             phone = phoneNumber,
             onPhoneChanged = { phoneNumber = it })
-
 
     }
 }
@@ -48,73 +62,114 @@ fun PhoneField(
     OutlinedTextField(
         value = phone,
         onValueChange = { it ->
-            onPhoneChanged(it.take(mask.count { it == maskChar }))
+            if (it.isDigitsOnly()) {
+                onPhoneChanged(it.take(mask.count { it == maskChar }))
+            }
         },
         label = {
             Text(text = "Phone number")
         },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-        visualTransformation = PhoneVisualTransformation(mask, maskChar) ,
-        modifier = modifier.fillMaxWidth(),
+        visualTransformation = { numberFilter(it) },
+        modifier = modifier
+            .fillMaxWidth()
+        ,
     )
 }
 
 
+fun numberFilter(text: AnnotatedString): TransformedText {
 
-class PhoneVisualTransformation(val mask: String, val maskChar: Char) : VisualTransformation {
+//    val maxLength = mask.count { it == maskChar }
 
-    //private val maxLength = mask.count { it == maskChar }
+//    val  trimmed = if (text.length > maxLength) text.take(maxLength) else text
+    val trimmed = if (text.length >= 10) text.text.substring(0..9) else text.text
 
-    override fun filter(text: AnnotatedString): TransformedText {
-        val trimmed = if (text.length >= 10) text.text.substring(0..9) else text.text
+    val annotatedString = buildAnnotatedString {
+//        if (trimmed.isEmpty()) return@buildAnnotatedString
 
-        val annotatedString = buildAnnotatedString {
-            if (trimmed.isEmpty()) return@buildAnnotatedString
-            var maskIndex = 0
-            var textIndex = 0
-            while (textIndex < trimmed.length && maskIndex < mask.length) {
-                if (mask[maskIndex] != maskChar) {
-                    val nextDigitIndex = mask.indexOf(maskChar, maskIndex)
-                    append(mask.substring(maskIndex, nextDigitIndex))
-                    maskIndex = nextDigitIndex
-                }
-                append(trimmed[textIndex++])
-                maskIndex++
+        if (trimmed.length < 10) {
+            pushStyle(SpanStyle(color = Color.Red))
+        }
+
+        var maskIndex = 0
+        for (trimIndex in trimmed.indices) {
+            if (mask[maskIndex] != maskChar) {
+                val nextMaskIndex = mask.indexOf(maskChar, maskIndex)
+                append(mask.substring(maskIndex, nextMaskIndex))
+                maskIndex = nextMaskIndex
+            }
+            append(trimmed[trimIndex])
+            maskIndex++
+        }
+//        pushStyle(SpanStyle(color = Color.LightGray))
+//        append(mask.takeLast(mask.length - this.length))
+    }
+
+    val phoneNumberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            var noneDigitCount = 0
+            var i = 0
+            while (i < offset + noneDigitCount) {
+                if (mask[i++] != maskChar) noneDigitCount++
+            }
+            return offset + noneDigitCount
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            return offset - mask.take(offset).count {
+                it != maskChar
             }
         }
 
-        return TransformedText(annotatedString, PhoneOffsetMapper())
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is PhoneVisualTransformation) return false
-        if (mask != other.mask) return false
-        if (maskChar != other.maskChar) return false
-        return true
-    }
+    return TransformedText(annotatedString, phoneNumberOffsetTranslator)
+}
 
-    override fun hashCode(): Int {
-        return mask.hashCode()
+class PhoneVisualTransformation(val mask: String, val maskNumber: Char) : VisualTransformation {
+
+    private val maxLength = mask.count { it == maskNumber }
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val trimmed = if (text.length > maxLength) text.take(maxLength) else text
+
+        val annotatedString = buildAnnotatedString {
+            if (trimmed.length < maxLength) {
+                pushStyle(SpanStyle(color = Color.Red))
+            }
+
+            var maskIndex = 0
+            for (textIndex in trimmed.indices) {
+                if (mask[maskIndex] != maskNumber) {
+                    val nextDigitIndex = mask.indexOf(maskNumber, maskIndex)
+                    append(mask.substring(maskIndex, nextDigitIndex))
+                    maskIndex = nextDigitIndex
+                }
+                append(trimmed[textIndex])
+                maskIndex++
+            }
+//            pushStyle(SpanStyle(color = Color.Red))
+//            append(mask.takeLast(mask.length - length))
+        }
+
+        return TransformedText(annotatedString, PhoneOffsetMapper(mask, maskNumber))
     }
 }
 
-private class PhoneOffsetMapper() : OffsetMapping {
+private class PhoneOffsetMapper(val mask: String, val numberChar: Char) : OffsetMapping {
 
     override fun originalToTransformed(offset: Int): Int {
-
         var noneDigitCount = 0
         var i = 0
         while (i < offset + noneDigitCount) {
-            if (mask[i++] != maskChar) noneDigitCount++
+            if (mask[i++] != numberChar) noneDigitCount++
         }
         return offset + noneDigitCount
     }
 
     override fun transformedToOriginal(offset: Int): Int =
-        offset - mask.take(offset).count {
-            it != maskChar
-        }
+        offset - mask.take(offset).count { it != numberChar }
 }
 
 
--- app/src/main/java/com/example/bottomnavigationexample/ui/screen/search/Search.kt
+++ app/src/main/java/com/example/bottomnavigationexample/ui/screen/search/Search.kt
@@ -1,36 +1,302 @@
 package com.example.bottomnavigationexample.ui.screen.search
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.bottomnavigationexample.R
+import kotlinx.coroutines.delay
+import kotlin.coroutines.coroutineContext
+
+private val phoneTextList = listOf(
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    ),
+    mutableStateOf(
+        TextFieldValue(
+            text = "",
+            selection = TextRange(0)
+        )
+    )
+)
+private val requesterList = listOf(
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+    FocusRequester(),
+)
 
 @Composable
 fun SearchScreen(navController: NavController) {
+
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.black))
-            .wrapContentSize(Alignment.Center)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+    ) {
+        ContentView()
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ContentView(
+
+) {
+
+    LaunchedEffect(null) {
+        delay(300L)
+        requesterList[0].requestFocus()
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
     ) {
         Text(
-            text = "Search Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+            text = "+90 (",
+            fontSize = 24.sp
+
         )
+        phoneTextList.forEachIndexed { index, mutableState ->
+            if (index == 3) {
+                Text(
+                    text = ") ",
+                    fontSize = 24.sp
+                )
+            }
+            if (index == 6) {
+                Text(text = "  ")
+            }
+            if (index == 8) {
+                Text(text = "  ")
+            }
+            InputPhoneView(
+                state = mutableState,
+                onValueChange = {
+                    if (mutableState.value.text != "") {
+                        if (it.text != mutableState.value.text) {
+                            mutableState.value = TextFieldValue(text = "", selection = TextRange(0))
+                            mutableState.value = TextFieldValue(
+                                text = (it.text.lastOrNull() ?: "").toString(),
+                                selection = TextRange(it.text.length)
+                            )
+                        } else {
+                            if (it.text == "") {
+                                mutableState.value =
+                                    TextFieldValue(text = "", selection = TextRange(0))
+                            }
+                            return@InputPhoneView
+                        }
+                    }
+                    mutableState.value = TextFieldValue(
+                        text = (it.text.lastOrNull() ?: "").toString(),
+                        selection = TextRange(it.text.length)
+                    )
+                    if (it.text != "") {
+                        nextFocus(index + 1, requesterList)
+                    }
+                },
+                focusRequester = requesterList[index],
+                index = index
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun InputPhoneView(
+    state: MutableState<TextFieldValue>,
+    index: Int,
+    onValueChange: (value: TextFieldValue) -> Unit,
+    focusRequester: FocusRequester
+) {
+    val focusState = remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val context = LocalContext.current
+
+    BasicTextField(
+        readOnly = false,
+        value = state.value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .padding(horizontal = 2.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .wrapContentSize()
+            .background(Color.LightGray)
+            .onFocusChanged {
+                focusState.value = it.isFocused
+            }
+            .onKeyEvent {
+                if (it.type == KeyEventType.KeyUp
+                    && it.key == Key.Backspace
+                    && focusState.value
+                ) {
+                    prevFocus(index - 1, requesterList)
+                }
+
+                false
+            }
+            .focusRequester(focusRequester),
+        maxLines = 1,
+        decorationBox = { innerTextField ->
+
+            Box(
+                modifier = Modifier
+                    .width(25.dp)
+                    .height(30.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                innerTextField()
+            }
+        },
+        cursorBrush = SolidColor(Color.Black),
+        textStyle = TextStyle(
+            color = Color.Black,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusState.value = false
+                focusManager.clearFocus()
+                keyboardController?.hide()
+                var number = ""
+                phoneTextList.forEachIndexed { index, mutableState ->
+                    number += mutableState.value.text
+                }
+                Toast.makeText(context,number,Toast.LENGTH_LONG).show()
+                Log.e("NUMBER", number)
+            }
+        )
+    )
+}
+
+private fun nextFocus(
+    index: Int,
+    requesterList: List<FocusRequester>,
+) {
+    if (index <= requesterList.size - 1) {
+        requesterList[index - 1].freeFocus()
+        requesterList[index].requestFocus()
+    }
+}
+
+private fun prevFocus(
+    index: Int,
+    requesterList: List<FocusRequester>
+) {
+    if (index >= 0) {
+        requesterList[index + 1].freeFocus()
+        requesterList[index].requestFocus()
     }
 }
